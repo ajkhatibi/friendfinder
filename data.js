2fe792bb2f375efa1b82ce6d01469eedceeabb76
Julian Khatibi <ajkhatibi@gmail.com>
2017-01-11 14:58:20 -0800

    friend finder

---
+++ data.js
@@ -0,0 +1,36 @@
+var friendsList = [
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},
+{
+  "name":"Akbar",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Julian_Hodgson_2001_Solingen.jpg/220px-Julian_Hodgson_2001_Solingen.jpg",
+  "scores":[
+     4,
+     3,
+     2,
+     1,
+     5,
+     1,
+     4,
+     5,
+     4,
+     2
+   ]
+}
+]
+
+module.exports = friendsList;
--- server.js
+++ server.js
@@ -5,10 +5,6 @@ var path = require('path');
 var app = express();
 var PORT = 3000;
 
-// require('./html-routes.js')(app)
-// require('./api-routes.js')(app)
-
-
 
 
 // Sets up the Express app to handle data parsing
@@ -18,51 +14,12 @@ app.use(bodyparser.text());
 app.use(bodyparser.json({ type: "application/vnd.api+json" }));
 
 ////pages
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
-
-app.get("/survey.html", function(req, res) {
-
-  res.sendFile(path.join(__dirname, "survey.html"));
-
-
-});
-
-
-app.post('/api/friends/?', function(req, res){
-	var friendsList = [{
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ]
-},
-
-]
-var friends = req.body;
-friendsList.push(friends)
-console.log(friendsList);
+require('./html-routes.js')(app)
+require('./api-routes.js')(app)
 
 
 
 
-
-
-
-
-
-
-})
 //running server
 app.listen(3000, function () {
   console.log('YODAWG')
